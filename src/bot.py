eaffd0337d6c2d9d728c3e29eef1d01d5279343d
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2022-12-12 11:39:08 +0800

    12121139

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
 import logging
 import json
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton ,KeyboardButton,ReplyKeyboardMarkup
-from telegram.ext import Updater, Filters,RegexHandler, CallbackContext,CommandHandler,MessageHandler,ConversationHandler
+from telegram.ext import Updater, Filters, CallbackContext,CommandHandler,MessageHandler,ConversationHandler
 import configparser
 
 logging.basicConfig(level=logging.DEBUG)
@@ -17,38 +17,42 @@ dispatcher = updater.dispatcher
 job_queue = updater.job_queue
 
 
-START,WORKFLOW = range(2)
+START,WORKFLOW,ADMINPANEL = range(3)
 
 
 with open("./json/keyBoardButton.json", 'r', encoding="utf-8") as f:
     dict = json.load(f)
 
+startKeyboardButton=[[KeyboardButton(dict['howToAddMeToYourGroup']),KeyboardButton(dict['managementPanel'])],
+        [KeyboardButton(dict['commandsList']),KeyboardButton(dict['supportGroup'])]]
+
+wordFlowKeyboardButton=[[KeyboardButton('禁言功能')],[KeyboardButton('频道信息清空')],[KeyboardButton('广告设置')],[KeyboardButton('返回')]]
+
 def start(update:Update,context:CallbackContext):
     chat_id = update.effective_chat.id
-    buttons=[[KeyboardButton(dict['howToAddMeToYourGroup']),KeyboardButton(dict['managementPanel'])],
-             [KeyboardButton(dict['commandsList']),KeyboardButton(dict['supportGroup'])]]
-    context.bot.send_message(chat_id=chat_id,text="What con this bot do?\nPlease tap on START",
-                                            reply_markup=ReplyKeyboardMarkup(buttons))
-    return WORKFLOW
+    userid=update.message.from_user.id
+    context.bot.send_message(chat_id=chat_id,text="What con this bot do?\nPlease tap on START",reply_markup=ReplyKeyboardMarkup(startKeyboardButton))
+    if str(chat_id) == str(userid):
+        return WORKFLOW
+    #else:
+    #    context.bot.send_message(chat_id=chat_id,text="开发中,请联系https://t.me/coffeeboy315")
 
 def wordFlow(update:Update,context:CallbackContext):
     chat_id = update.effective_chat.id
     text = update.message.text
+    username = update.message.from_user.first_name
     if text == dict['howToAddMeToYourGroup']:
-        context.bot.send_message(chat_id=chat_id,
-            text=f'Tap on this link and then choose your group.\n\n{addLink}\n\n"Add admins" permission is required.',
+        context.bot.send_message(chat_id=chat_id , text=f'Tap on this link and then choose your group.\n\n{addLink}\n\n"Add admins" permission is required.',
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Add to group', url=addLink)]]))
 
     if text == dict['managementPanel']:
         userid=update.message.from_user.id
         for m in manager:
-            if str(m) in str(userid):
-                buttons=[[KeyboardButton('查詢管理員')],[KeyboardButton('新增管理員')],[KeyboardButton('刪除管理員')],[KeyboardButton('返回')]]
-                #context.bot.send_message(chat_id=chat_id,text="認證完成",reply_markup=ReplyKeyboardMarkup(buttons))
-                ReplyKeyboardMarkup(buttons)
-                break
+            if str(m) == str(userid):
+                context.bot.send_message(chat_id=chat_id,text=f"Account {username} uses the administrator function",reply_markup=ReplyKeyboardMarkup(wordFlowKeyboardButton))
+                return ADMINPANEL
 
-
+        context.bot.send_message(chat_id=chat_id,text='目前尚无权限,请联系https://t.me/coffeeboy315')
     if text == dict['commandsList']:
         context.bot.send_message(chat_id=chat_id,text='undeveloped')
     if text == dict['supportGroup']:
@@ -56,13 +60,22 @@ def wordFlow(update:Update,context:CallbackContext):
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Enter group', url=groupLink)]]))
     return WORKFLOW
 
+def adminPanel(update:Update,context:CallbackContext):
+    chat_id = update.effective_chat.id
+    text = update.message.text
+    if text == '返回':
+        context.bot.send_message(chat_id=chat_id,text="What con this bot do?\nPlease tap on START",reply_markup=ReplyKeyboardMarkup(startKeyboardButton))
+        return START
+    return ADMINPANEL
+
 bot_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start),MessageHandler(filters=Filters.text & (~ Filters.command), callback=wordFlow)],
     states={
         START:[CommandHandler('start', start)],
         WORKFLOW: [MessageHandler(filters=Filters.text & (~ Filters.command), callback=wordFlow)],
+        ADMINPANEL: [MessageHandler(filters=Filters.text & (~ Filters.command), callback=adminPanel)],
     },
-    fallbacks=[CommandHandler('start', start)])
+    fallbacks=[CommandHandler('start', start),MessageHandler(filters=Filters.text & (~ Filters.command), callback=wordFlow)])
 dispatcher.add_handler(bot_handler)
 
 def run():
