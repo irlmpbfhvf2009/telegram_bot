2ba508e17909cc83e21a3ef02b8cd09156a66283
leo <hyprocrite1631@gmail.com>
2023-01-11 13:19:57 +0800

    add pyinstaller

--- app.py
+++ app.py
@@ -8,21 +8,7 @@ class FlaskThread(threading.Thread):
     def run(self) -> None:
         app.run(host="0.0.0.0")
 
-class TelegramThread(threading.Thread):
-    def run(self) -> None:
-        try:
-            bot.run()
-        except:
-            ...
-
 @app.route("/")
 def index():
     return render_template('index.html')
 
-
-
-if __name__ == '__main__':
-    flask_thread = FlaskThread()
-    telegram_thread = TelegramThread()
-    flask_thread.start()
-    telegram_thread.start()
